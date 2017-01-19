948ab7fa7535c21469b622eb3e141790bd82df4c
Jordan Lapslie <jlapslie@gmail.com>
2017-01-19 16:40:59 +1300

    Upgraded to Silverstripe 4

--- code/FieldCounterCMSExtension.php
+++ code/FieldCounterCMSExtension.php
@@ -1,18 +1,27 @@
 <?php
+
+namespace FieldCounter;
+
+use SilverStripe\Admin\LeftAndMainExtension;
+use SilverStripe\Forms\TextField;
+use SilverStripe\View\Requirements;
+
 class FieldCounterCMSExtension extends LeftAndMainExtension
 {
+
+
+    private function ModuleBase()
+    {
+        return basename(dirname(dirname(__FILE__)));
+    }
 
     public function init()
     {
         parent::init();
 
         Requirements::css($this->ModuleBase() . '/css/counter.css');
-
         Requirements::javascript($this->ModuleBase() . '/javascript/counter.js');
     }
 
-    private function ModuleBase()
-    {
-        return basename(dirname(dirname(__FILE__)));
-    }
+
 }
